f0ae582e8f967552d5842ebc63595d963e872de0
Faiyaz Rafeek <fayaznahwi@gmail.com>
2020-11-18 13:51:15 +0530

    all math functions add

--- GUI.py
+++ GUI.py
@@ -6,6 +6,7 @@ root.title("Simple Calculator")
 e = Entry(root, width=35, borderwidth = 1)
 e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
+
 def btnClick(number):
     current = e.get()
     e.delete(0, END)
@@ -16,15 +17,53 @@ def btnClear():
     e.delete(0, END)
 
 def btnAdd():
+    global math
+    math = "add"
+    fNum = e.get()
     global num1
-    num1 = e.get()
+    num1 = int(fNum)
+    e.delete(0, END)
+
+def btnSub():
+    global math
+    math = "sub"
+    fNum = e.get()
+    global num1
+    num1 = int(fNum)
+    e.delete(0, END)
+
+def btnMul():
+    global math
+    math = "mul"
+    fNum = e.get()
+    global num1
+    num1 = int(fNum)
+    e.delete(0, END)
+
+def btnDiv():
+    global math
+    math = "div"
+    fNum = e.get()
+    global num1
+    num1 = int(fNum)
     e.delete(0, END)
 
 def btnEqual():
-    num2 = str(e.get())
+    num2 = int(e.get())
     e.delete(0,END)
-    sum = int(num1) + int(num2)
-    e.insert(0, sum)
+
+    if math=="add":
+        sum = num1 + num2
+        e.insert(0, sum)
+    elif math=="sub":
+        sum = num1 - num2
+        e.insert(0, sum)
+    elif math=="mul":
+        sum = num1 * num2
+        e.insert(0, sum)
+    else:
+        sum = num1 / num2
+        e.insert(0, sum)
 
 
 btn1 = Button(root, text="1", padx=30, pady=20, command=lambda: btnClick(1))
@@ -40,6 +79,10 @@ btn0 = Button(root, text="0", padx=30, pady=20, command=lambda: btnClick(0))
 btnAdd = Button(root, text="+", padx=29, pady=20, command= btnAdd)
 btnEqual = Button(root, text="=", padx=68, pady=20, command= btnEqual)
 btnClear = Button(root, text="Clear", padx=58, pady=20,command=btnClear)
+
+btnSub = Button(root, text="-", padx=30, pady=20, command= btnSub)
+btnMul = Button(root, text="*", padx=30, pady=20, command= btnMul)
+btnDiv = Button(root, text="/", padx=30, pady=20, command= btnDiv)
 
 
 
@@ -59,6 +102,9 @@ btn0.grid(row=4, column=0)
 btnClear.grid(row=4, column=1, columnspan=2)
 btnAdd.grid(row=5, column=0)
 btnEqual.grid(row=5, column=1, columnspan=2)
+btnSub.grid(row=6, column=0)
+btnMul.grid(row=6, column=1)
+btnDiv.grid(row=6, column=2)
 
 
 
